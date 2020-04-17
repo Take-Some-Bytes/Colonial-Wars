37e0415f731493cfe65d53b5c3a4f23498714bbc
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-04-16 20:59:54 -0700

    Update Winstoninit.js

--- Lib/Winstoninit.js
+++ Lib/Winstoninit.js
@@ -9,38 +9,38 @@ const { combine, timestamp, label, printf } = winston.format;
 
 // eslint-disable-next-line no-shadow
 const myFormat = printf(({ level, message, label, timestamp }) => {
-   return `${timestamp} [${label}] ${level}: ${message}`;
+  return `${timestamp} [${label}] ${level}: ${message}`;
 });
 
 //Add CSP-report logger
 winston.loggers.add("CSP-logger", {
-   levels: winston.config.syslog.levels,
-   format: combine(
-      label({ label: "CSP-report" }),
-      timestamp(),
-      myFormat
-   ),
-   transports: [
-      new winston.transports.File({
-         filename: "logs/CSP-reports.log",
-         level: ""
-      })
-   ]
+  levels: winston.config.syslog.levels,
+  format: combine(
+    label({ label: "CSP-report" }),
+    timestamp(),
+    myFormat
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: "logs/CSP-reports.log",
+      level: ""
+    })
+  ]
 });
 //Add Server logger
 winston.loggers.add("Server-logger", {
-   levels: winston.config.syslog.levels,
-   format: combine(
-      label({ label: "Server" }),
-      timestamp(),
-      myFormat
-   ),
-   transports: [
-      new winston.transports.File({
-         filename: "logs/Server-logs.log",
-         level: ""
-      })
-   ]  
+  levels: winston.config.syslog.levels,
+  format: combine(
+    label({ label: "Server" }),
+    timestamp(),
+    myFormat
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: "logs/Server-logs.log",
+      level: ""
+    })
+  ]  
 })
 
 const loggers = winston.loggers;
