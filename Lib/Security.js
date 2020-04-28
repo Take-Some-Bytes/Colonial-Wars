d73d72e05bb22ee83b5e6ea407cdab9fdaea8714
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-04-28 12:44:51 -0700

    Update Security.js

--- Lib/Security.js
+++ Lib/Security.js
@@ -2,7 +2,6 @@
  * @fileoverview This is a Security class, made for
  * enforcing security on a http or https server with the express framework.
  * @author Horton Cheng <horton0712@gmail.com>
- * @version 1.0.1
  */
 
 //Dependencies
@@ -17,10 +16,10 @@ class Security {
     * Security class for enforcing security on a https or http server.
     * Please only use if you are using an express server
     * @constructor
-    * @param {Array<string>} allowed_methods allowed methods 
+    * @param {Array<string>} allowed_methods allowed methods
     * for accessing the server
     * @param {Boolean} HTTPS Is the server a https server?
-    * @param {Boolean} HSTS_PRLD_LIST_SUBM is the server 
+    * @param {Boolean} HSTS_PRLD_LIST_SUBM is the server
     * registered with the HSTS Preload List Submission?
     */
   constructor(allowed_methods, HTTPS, HSTS_PRLD_LIST_SUBM) {
@@ -36,14 +35,14 @@ class Security {
     */
   setDefaultHeaders(req, res) {
     res.set({
-      "Content-Security-Policy": 
+      "Content-Security-Policy":
         "default-src 'self'; script-src 'self' https://ajax.googleapis.com/; " +
         "style-src 'self' https://*.googleapis.com/ https://*.gstatic.com/; " +
-        "font-src https://*.googleapis.com/ https://*.gstatic.com/; " + 
-        "img-src 'self'; child-src 'self'; media-src 'none'; " + 
-        "object-src 'none'; base-uri 'self'; " + 
-        "connect-src 'self' https://*.googleapis.com/ " + 
-        "https://*.gstatic.com/; report-uri logs/CSP-reports.log; " + 
+        "font-src https://*.googleapis.com/ https://*.gstatic.com/; " +
+        "img-src 'self'; child-src 'self'; media-src 'none'; " +
+        "object-src 'none'; base-uri 'self'; " +
+        "connect-src 'self' https://*.googleapis.com/ " +
+        "https://*.gstatic.com/; report-uri logs/CSP-reports.log; " +
         "frame-ancestors 'none'; form-action 'self'",
       "X-XSS-Protection": "1; mode=block",
       "X-Content-Type-Options": "nosniff"
@@ -71,17 +70,17 @@ class Security {
   enforceHTTPS(req, res) {
     if(!this.HTTPS || !this.HSTS_PRLD_LIST_SUBM) {
       throw new Error(
-        "Header is only for HTTPS " + 
+        "Header is only for HTTPS " +
         "servers registered with the HSTS preload list submission."
       );
     }
     res.set(
-      "Strict-Transport-Security", 
+      "Strict-Transport-Security",
       "max-age=31536000; includeSubDomains; preload"
     );
   }
   /**
-    * Checks the requested method, 
+    * Checks the requested method,
     * and sees if it follows the allowed methods array.
     * Returns a string reporting whether there is an error or not.
     * @param {request} req Client request
@@ -91,14 +90,14 @@ class Security {
   checkMethod(req, res) {
     for (let i = 0; i < this.allowed_methods.length; i++) {
       if(// eslint-disable-next-line eqeqeq
-        req.method != this.allowed_methods[i] && 
+        req.method != this.allowed_methods[i] &&
         // eslint-disable-next-line eqeqeq
         i == this.allowed_methods.length - 1
       ) {
-        res.type("html");            
+        res.type("html");
         res.status(501)
           .send(
-            "<h1>Method Not Implemented</h1>\n" + 
+            "<h1>Method Not Implemented</h1>\n" +
             "<h3>That method is not implemented.</h3>"
           );
         this.status = "error";
@@ -115,7 +114,7 @@ class Security {
     * Returns a string reporting whether there is an error or not.
     * @param {request} req Client request
     * @param {response} res Server response
-    * @param {Array<string>} allowed_paths The paths that you will 
+    * @param {Array<string>} allowed_paths The paths that you will
     * allow clients to request for
     * @param {String} reqFile The requested file
     * @returns {String}
@@ -123,29 +122,29 @@ class Security {
   checkRequest(req, res, allowed_paths, reqFile) {
     for(let i = 0; i < allowed_paths.length; i++) {
       if(
-        reqFile.indexOf(allowed_paths[i] + path.sep) !== 0 && 
+        reqFile.indexOf(allowed_paths[i] + path.sep) !== 0 &&
         i === allowed_paths.length - 1
       ) {
         res.type("html");
         res.status(403)
           .send(
-            "<h1>That is forbidden.</h1>\n" + 
+            "<h1>That is forbidden.</h1>\n" +
             "<h3>The requested page is not for the public.</h3>"
           );
         this.status = "error";
         return this.status;
       } else if(reqFile.indexOf(allowed_paths[i] + path.sep) === 0) {
         this.status = "success";
-        return this.status;   
+        return this.status;
       }
     }
-  }  
+  }
   /**
     * Factory method for the Security class
-    * @param {Array<string>} allowed_methods allowed methods 
+    * @param {Array<string>} allowed_methods allowed methods
     * for accessing the server
     * @param {Boolean} HTTPS Is the server a https server?
-    * @param {Boolean} HSTS_PRLD_LIST_SUBM is the server registered with the 
+    * @param {Boolean} HSTS_PRLD_LIST_SUBM is the server registered with the
     * HSTS Preload List Submission?
     * @returns {Security}
     */
