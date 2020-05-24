595007d53a9be45786100be1f327ba9a01501dd1
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-05-23 21:29:06 -0700

    Lots more methods added for Manager.js

--- Lib/Game/Manager.js
+++ Lib/Game/Manager.js
@@ -6,6 +6,8 @@
 const Game = require("./Game");
 const Vector = require("./Physics/Vector");
 const Constants = require("../Constants");
+const crypto = require("crypto");
+const socketIO = require("socket.io");
 const { deepClear } = require("../Util");
 
 /**
@@ -37,6 +39,17 @@ class Manager {
     return this.clients.size;
   }
   /**
+   * Gets all the games as an array
+   * @returns {Array<Game>}
+   */
+  get allGames() {
+    const games = [];
+    for(const game of this.games.values()) {
+      games.push(game);
+    }
+    return games;
+  }
+  /**
     * Initializes the Manager state
     */
   init() {
@@ -47,15 +60,56 @@ class Manager {
   }
   /**
     * Adds a new client
-    * @param {Object} socket The socket object associated with this client
+    * @param {socketIO.Socket} socket The socket object associated with
+    * this client
+    * @param {{
+    * token: String
+    * }} session The session of the client
     */
-  addNewClient(socket) {
-    this.clients.set(socket.id, socket);
+  addNewClient(socket, session) {
+    this.clients.set(socket.id, {
+      clientID: socket.id,
+      clientToken: session.token,
+      socket: socket
+    });
+  }
+  /**
+   * Removes a client
+   * @param {String} socketID The socket ID associated with this client
+   */
+  removeClient(socketID) {
+    if(this.clients.has(socketID)) {
+      this.clients.delete(socketID);
+    }
+  }
+  /**
+   * Changes a client's stats
+   * @param {{
+   * id: String,
+   * token: String,
+   * socket: socketIO.Socket
+   * }} newStats The new stats to give the client
+   * @param {String} ID The ID of the client. Must be the original ID
+   */
+  changeStats(newStats, ID) {
+    let client = {};
+    if(!this.clients.has(ID)) {
+      throw new Error("Client does not exist!");
+    }
+    client = {
+      clientID: newStats.id,
+      clientToken: newStats.token,
+      socket: newStats.socket
+    }
+    this.clients.delete(ID);
+    this.clients.set(newStats.id, client);
   }
   /**
     * Adds a new game, then returns the made game
     * @param {String} mode The game mode
     * @param {String} gameID The game ID
+    * @param {String} mapName The name of the map that the game is
+    * going to take place in
     * @param {{
     * British: Vector,
     * French: Vector,
@@ -67,11 +121,12 @@ class Manager {
     * that join a team
     * @returns {Game}
     */
-  addNewGame(mode, gameID, startPositions) {
+  addNewGame(mode, gameID, mapName, startPositions) {
     if(this.games.size === Constants.MAX_GAMES) {
       throw new Error("Max number of games is reached; cannot add game");
     }
-    const game = new Game(mode, gameID, startPositions);
+    const token = crypto.randomBytes(16).toString("hex");
+    const game = new Game(mode, gameID, token, mapName, startPositions);
     this.games.set(gameID, game);
     return game;
   }
@@ -104,6 +159,54 @@ class Manager {
     for(const game of this.games.values()) {
       game.update();
     }
+  }
+  /**
+   * Sends the player state to all clients
+   */
+  sendState() {
+    for(const game of this.games.values()) {
+      game.sendState();
+    }
+  }
+  /**
+   * Adds a new client to the specified game
+   * @param {String} gameID The game's ID
+   * @param {socketIO.Socket} client The socket object associated with the
+   * client
+   * @param {String} name The display name of the client
+   * @param {String} team The team of the player
+   */
+  addClientToGame(gameID, client, name, team) {
+    if(this.games.has(gameID)) {
+      const game = this.games.get(gameID);
+      game.addNewPlayer(client, name, team);
+      return;
+    }
+    throw new Error("Game does not exist; cannot add player to game.");
+  }
+  /**
+   * Removes a client from the specified game
+   * @param {String} gameID The game's ID
+   * @param {socketIO.Socket} client The socket object associated with the
+   * client
+   */
+  removeClientFromGame(gameID, client) {
+    if(this.games.has(gameID)) {
+      const game = this.games.get(gameID);
+      game.removePlayer(client.id);
+    }
+  }
+  /**
+   * Gets a specific client
+   * @param {String} ID The ID associated with the client
+   * @returns {{}}
+   */
+  getClient(ID) {
+    const client = this.clients.get(ID);
+    if(!client) {
+      return {};
+    }
+    return client;
   }
   /**
     * Create a new game manager
