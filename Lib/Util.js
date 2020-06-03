448b71b2711615532a6cacecfecd1e1b74bbc99d
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-06-02 21:27:45 -0700

    Update Util.js
    Stylistic updates and a new method

--- Lib/Util.js
+++ Lib/Util.js
@@ -15,7 +15,7 @@ const Vector = require("./Game/Physics/Vector");
  * @returns {Boolean}
  */
 function inBound(val, min, max) {
-  if(min > max) { return val >= max && val <= min; }
+  if (min > max) { return val >= max && val <= min; }
   return val >= min && val <= max;
 }
 /**
@@ -45,7 +45,7 @@ function degreeToRadian(degree) {
  * @return {number}
  */
 function normalizeAngle(angle) {
-  while(angle < 0) {
+  while (angle < 0) {
     angle += Math.PI * 2;
   }
   return angle % (Math.PI * 2);
@@ -60,7 +60,7 @@ function makeID(length) {
   const characters =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
-  for(let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
@@ -75,10 +75,10 @@ function deepFreeze(object) {
   const propNames = Object.getOwnPropertyNames(object);
 
   // Freeze properties before freezing self
-  for(const name of propNames) {
+  for (const name of propNames) {
     const value = object[name];
 
-    if(value && typeof value === "object") {
+    if (value && typeof value === "object") {
       deepFreeze(value);
     }
   }
@@ -93,30 +93,30 @@ function deepFreeze(object) {
  * @returns {*}
  */
 function deepClear(variable, deleteProperties = false) {
-  if(variable instanceof Map) {
-    if(deleteProperties) {
+  if (variable instanceof Map) {
+    if (deleteProperties) {
       return variable.clear();
     }
-    for(const [key, value] of variable) {
-      if(value instanceof Map) {
+    for (const [key, value] of variable) {
+      if (value instanceof Map) {
         deepClear(value, deleteProperties);
       } else {
         variable.set(key, undefined);
       }
     }
-  } else if(variable instanceof Array) {
-    if(deleteProperties) {
+  } else if (variable instanceof Array) {
+    if (deleteProperties) {
       return variable.splice(0);
     }
-    for(let i = 0; i < variable.length; i++) {
+    for (let i = 0; i < variable.length; i++) {
       variable[i] = undefined;
       continue;
     }
-  } else if(typeof variable === "object") {
+  } else if (typeof variable === "object") {
     const properties = Object.getOwnPropertyNames(variable);
-    for(let i = 0; i < properties.length; i++) {
+    for (let i = 0; i < properties.length; i++) {
       const property = variable[properties[i]];
-      if(deleteProperties) {
+      if (deleteProperties) {
         delete variable[property];
         continue;
       } else {
@@ -143,20 +143,20 @@ function multiplySomething(factors) {
   let product = 0;
   let i = 0;
 
-  while(i < factors.length) {
-    if(!preProduct) {
-      if(!factor1) {
+  while (i < factors.length) {
+    if (!preProduct) {
+      if (!factor1) {
         factor1 = factors[i];
         i++;
         continue;
-      } else if(!factor2) {
+      } else if (!factor2) {
         factor2 = factors[i];
         preProduct = factor1 * factor2;
-        if(!preProduct) {
-          if(factor1) {
+        if (!preProduct) {
+          if (factor1) {
             preProduct = factor1;
             break;
-          } else if(factor2) {
+          } else if (factor2) {
             preProduct = factor2;
             break;
           }
@@ -172,7 +172,7 @@ function multiplySomething(factors) {
       }
     } else {
       factor = factors[i];
-      if(!factor) {
+      if (!factor) {
         i++;
         continue;
       }
@@ -192,9 +192,9 @@ function multiplySomething(factors) {
  * @returns {Boolean}
  */
 function checkProperties(obj) {
-  for(const key in obj) {
+  for (const key in obj) {
     const property = obj[key];
-    if(!property === true) {
+    if (!property === true) {
       return false;
     }
   }
@@ -220,7 +220,7 @@ function getEuclideanDist2(p1, p2) {
     (p2.y - p1.y) * (p2.y - p1.y)
   );
 
-  if(isNaN(distance)) {
+  if (isNaN(distance)) {
     throw new TypeError(
       "Arguments are not valid Vectors, or " +
       "the properties of the Vectors are not valid numbers!"
@@ -242,15 +242,39 @@ function inCircle(circlePosition, circleRadius, pointPosition) {
   return isInCircle;
 }
 /**
- * Logs the memory usage of the node js current process
+ * Logs the memory usage of the current node js process
  */
 function logMemoryUsage() {
   console.log("----Memory Usage----");
   const used = process.memoryUsage();
-  for(const key in used) {
+  for (const key in used) {
     console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
   }
   console.log("");
+}
+
+/**
+ * Gets all the non-callable (Non-function) properties of an object
+ * @param {Object} obj The object to get the non-callable properties
+ * from
+ * @returns {Object}
+ */
+function getNonCallableProps(obj) {
+  const propsNames = Object.getOwnPropertyNames(obj);
+  const props = {};
+
+  for (let i = 0; i < propsNames.length; i++) {
+    props[propsNames[i]] = obj[propsNames[i]];
+  }
+
+  for (const key in props) {
+    const prop = props[key];
+    if (!(typeof prop !== "function")) {
+      delete props[key];
+    }
+  }
+
+  return props;
 }
 
 module.exports = exports = {
@@ -266,5 +290,6 @@ module.exports = exports = {
   delay,
   getEuclideanDist2,
   inCircle,
-  logMemoryUsage
+  logMemoryUsage,
+  getNonCallableProps
 };
