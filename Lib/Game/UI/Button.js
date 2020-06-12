23b0dc1c0dd3fc70c491ac5b3381f630167cdfb2
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-06-11 21:12:11 -0700

    Updated Button.js to inherit from UIElement class

--- Lib/Game/UI/Button.js
+++ Lib/Game/UI/Button.js
@@ -5,11 +5,13 @@
 
 const Vector = require("../Physics/Vector");
 const Constants = require("../../Constants");
+const UIElement = require("./UIElement");
 
 /**
  * Button class
+ * @extends UIElement
  */
-class Button {
+class Button extends UIElement {
   /**
    * Constructor for a Button class
    * @param {{
@@ -24,32 +26,22 @@ class Button {
    * }} config Button config
    */
   constructor(config) {
-    this.width = config.width || 133;
-    this.height = config.height || 25;
-    this.image = config.image || undefined;
-    this.position = config.position;
+    super({
+      width: config.width,
+      height: config.height,
+      image: config.image,
+      position: config.position,
+      clickable: true,
+      value: null,
+      onHover: config.onHover,
+      onNotHover: config.onNotHover
+    });
+
     this.onClick = config.onClick;
-    this.onHover = config.onHover;
     this.onNotClick = config.onNotClick;
-    this.onNotHover = config.onNotHover;
+
     this.clicked = false;
-    this.hovered = false;
-    this.clickable = true;
     this.lastClickTime = 0;
-  }
-  /**
-   * Test if the mouse is inside the button
-   * @param {{
-   * mouseX: Number,
-   * mouseY: Number
-   * }} event The mouse event that happened
-   * @returns {Boolean}
-   */
-  isMouseInside(event) {
-    return event.mouseX > this.position.x &&
-        event.mouseX < this.position.x + this.width &&
-        event.mouseY > this.position.y &&
-        event.mouseY < this.position.y + this.height;
   }
   /**
    * Returns a boolean based on whether this button could be
@@ -83,9 +75,9 @@ class Button {
       this.clicked && this.clickable
     ) {
       this.onNotClick();
-    } else if (isMouseInside && !event.clicked && this.clickable) {
+    } else if (isMouseInside && !event.clicked) {
       this.onHover();
-    } else if (!isMouseInside && this.clickable) {
+    } else if (!isMouseInside) {
       this.onNotHover();
     }
   }
