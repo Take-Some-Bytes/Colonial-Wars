1d8cce156262d5a7f070011a8beb3177ce9d407d
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-08-16 12:43:07 -0700

    v0.3.8: Update version, code in Constants-client.js

--- Public/JS/Constants-client.js
+++ Public/JS/Constants-client.js
@@ -6,12 +6,12 @@
 import { deepSeal } from "./common/functions.js";
 
 const Constants = {
-  //World max and min
+  // World max and min
   WORLD_MIN: -4500,
   WORLD_MAX: 4500,
-  //Version
-  VERSION: "0.8.6-ALPHA",
-  //Drawing
+  // Version
+  VERSION: "0.3.8-ALPHA",
+  // Drawing
   DRAWING_TROOP_BASE_PATH: "/imgs/troops",
   DRAWING_BUILDING_BASE_PATH: "/imgs/buildings",
   DRAWING_OBSTACLE_BASE_PATH: "/imgs/obstacles",
@@ -57,10 +57,10 @@ const Constants = {
     ammo: [0, 0],
     people: [0, 1]
   },
-  //Viewport
+  // Viewport
   VIEWPORT_STICKINESS: 0.004,
-  //Viewport
-  VIEWPORT_WIDTH: (function() {
+  // Viewport
+  VIEWPORT_WIDTH: (() => {
     if (window.innerWidth !== undefined) {
       const vw = window.innerWidth;
       return vw;
@@ -68,7 +68,7 @@ const Constants = {
     const vw = document.documentElement.clientWidth;
     return vw;
   })(),
-  VIEWPORT_HEIGHT: (function() {
+  VIEWPORT_HEIGHT: (() => {
     if (window.innerHeight !== undefined) {
       const vh = window.innerHeight;
       return vh;
@@ -77,11 +77,11 @@ const Constants = {
     return vh;
   })(),
 
-  //Communications
+  // Communications
   SOCKET_UPDATE: "update",
   SOCKET_NEW_PLAYER: "new-player",
   SOCKET_PLAYER_ACTION: "player-action",
-  /*SOCKET_CHAT_CLIENT_SERVER: "chat-client-to-server",
+  /* SOCKET_CHAT_CLIENT_SERVER: "chat-client-to-server",
   SOCKET_CHAT_SERVER_CLIENT: "chat-server-to-client",*/
   SOCKET_DISCONNECT: "disconnect",
   SOCKET_AVAILABLE_GAMES: "available-games",
