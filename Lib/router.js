452ece6c808fdc8ba0af760c74f72c16eee20e1d
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-08-15 21:51:33 -0700

    v0.3.8: Update router.js
    Remove some useless functions, clean up.

--- Lib/router.js
+++ Lib/router.js
@@ -3,33 +3,23 @@
  * @author Horton Cheng <horton0712@gmail.com>
  */
 
-//Dependencies and stuff
+// Dependencies and stuff
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
 const init = require("./common/init");
 const {
-  serveFile, methodNotImplemented, handleOther, logCSPReport, methodNotAllowed
+  serveFile, handleOther, logCSPReport, sendError
 } = require("./common/common");
 
 const router = express.Router();
 
-const security = init.security;
 const manager = init.manager;
-const loggers = init.winstonLoggers;
 const morganLoggers = init.morganLoggers;
-const ServerLogger = loggers.get("Server-logger");
-const SecurityLogger = loggers.get("Security-logger");
 
-//Route logger and security thing
+// Route logger.
 router.use(morganLoggers.consoleLogger, morganLoggers.fileLogger);
-router.use((req, res, next) => {
-  //Security
-  security.setDefaultHeaders(req, res);
-  next();
-});
 
-//Homepage
+// Homepage
 router.route("/")
   .get((req, res, next) => {
     const filePath = path.join(
@@ -41,12 +31,9 @@ router.route("/")
       "<h1>Page Not Found.</h1>\n" +
       "<h3>Somehow the home page isn't where it used to be</h3>"
     );
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//Game page
+// Game page
 router.route("/play")
   .get((req, res, next) => {
     const filePath = path.join(
@@ -58,12 +45,9 @@ router.route("/play")
       "<h1>Page Not Found.</h1>\n" +
       "<h3>Somehow the play page isn't where it used to be</h3>"
     );
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//About page
+// About page
 router.route("/about")
   .get((req, res, next) => {
     const filePath = path.join(
@@ -75,12 +59,9 @@ router.route("/about")
       "<h1>Page Not Found.</h1>\n" +
       "<h3>Somehow the about page isn't where it used to be</h3>"
     );
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//Version page
+// Version page
 router.route("/version")
   .get((req, res, next) => {
     const filePath = path.join(
@@ -92,12 +73,9 @@ router.route("/version")
       "<h1>Page Not Found.</h1>\n" +
       "<h3>Somehow the version page isn't where it used to be</h3>"
     );
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//License Page
+// License Page
 router.route("/license")
   .get((req, res, next) => {
     const filePath = path.join(
@@ -109,50 +87,52 @@ router.route("/license")
       "<h1>Page Not Found.</h1>\n" +
     "<h3>Somehow the license page isn't where it used to be</h3>"
     );
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//CSP report uri
+// CSP report uri
 router.route("/CSP-report")
-  .get((req, res, next) => methodNotAllowed(req, res, next, ["POST"]))
   .post((req, res, next) => {
     logCSPReport(req, res, next);
-  })
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//XHR route
+// XHR route
 router.route("/xhr")
   .get((req, res, next) => {
     const query = req.query;
-    const date = new Date();
-    const cookies = req.signedCookies;
+    const clientIP = req.ips.length < 1 ?
+      req.ip :
+      req.ips[0];
 
-    //Security
+    // Security
     if (Object.keys(query).length === 0 && query.constructor === Object) {
-      SecurityLogger.notice(
-        `Someone tried to get: ${req.url} without a query. ` +
-        `Request date: ${date}`
-      );
-      res
-        .status(400)
-        .send("No query supplied! Cannot process request.");
+      sendError({
+        httpOpts: {
+          status: 400
+        },
+        logOpts: {
+          loggerID: "Server-Logger",
+          logLevel: "notice",
+          doLog: true,
+          logMessage:
+            `Someone tried to get: ${req.url} without a query. 
+            Client IP: ${clientIP}.`
+        }
+      })(req, res, next);
       return;
-    } else if (typeof query.for === "undefined") {
-      SecurityLogger.notice(
-        `Someone tried to get ${req.url} without a for field in their query.` +
-        ` Request date: ${date}`
-      );
-    } else if (!cookies.token || !cookies.clientID) {
-      SecurityLogger.notice(
-        `Someone tried to get: ${req.url} without a token or ID with their ` +
-        `cookies. Request date: ${date}`
-      );
-      res
-        .status(401)
-        .send("No token or ID specified in cookies!");
+    } else if (typeof query.for !== "string") {
+      sendError({
+        httpOpts: {
+          status: 400
+        },
+        logOpts: {
+          loggerID: "Server-Logger",
+          logLevel: "notice",
+          doLog: true,
+          logMessage:
+            `Someone tried to get: ${req.url} without a for field in their 
+             query. Client IP: ${clientIP}.`
+        }
+      })(req, res, next);
       return;
     }
 
@@ -182,59 +162,37 @@ router.route("/xhr")
       break;
     }
     case "license_text.html": {
-      const s = fs.createReadStream(
+      serveFile(
+        req, res, next,
         path.join(
           __dirname, "../",
           "Public/license_text.html"
-        )
+        ), true
       );
-      let data = "";
-      s.on("data", chunk => {
-        data += chunk;
-      });
-      s.on("end", () => {
-        const response = JSON.stringify({
-          status: 200,
-          html: data
-        });
-        res.send(response);
-      });
-      s.on("error", err => {
-        ServerLogger.error(err);
-        if (err.code === "ENOENT") {
-          res
-            .status(404)
-            .send("File not found.");
-        } else {
-          res
-            .status(500)
-            .send("Server error");
-        }
-      });
       break;
     }
     default:
-      SecurityLogger.notice(
-        `Someone tried to get: ${req.url} with an unrecognized for field. ` +
-        `Request date: ${date}.`
-      );
-      res
-        .status(400)
-        .send("Unrecognized for field! Cannot process request.");
+      sendError({
+        httpOpts: {
+          status: 400
+        },
+        logOpts: {
+          loggerID: "Server-Logger",
+          logLevel: "notice",
+          doLog: true,
+          logMessage:
+            `Someone tried to get: ${req.url} with an unrecognized for field.
+            Client IP: ${clientIP}.`
+        }
+      })(req, res, next);
     }
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 
-//All other routes
+// All other routes
 router.route("*")
   .get((req, res, next) => {
     handleOther(req, res, next);
-  })
-  .post((req, res, next) => methodNotAllowed(req, res, next, ["GET"]))
-  .put((req, res, next) => methodNotImplemented(req, res, next))
-  .delete((req, res, next) => methodNotImplemented(req, res, next));
+  });
 /**
  * Module exports
  */
