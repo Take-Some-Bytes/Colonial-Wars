d05ae387da7bb9088728d9f5d71aabc016bb1cb7
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-09-08 18:04:59 -0700

    v0.4.1: Updated and added comments in UIElement.js

--- Lib/Game/UI/UIElement.js
+++ Lib/Game/UI/UIElement.js
@@ -3,26 +3,29 @@
  * @author Horton Cheng <horton0712@gmail.com>
  */
 
+// Imports.
 const Vector = require("../Physics/Vector");
 
+// TODO: Move this class to the client-side.
 /**
- * UI element class
+ * UI element class.
  */
 class UIElement {
   /**
-   * Constructor for a UIElement class
+   * Constructor for a UIElement class.
+   * @class
    * @param {{
-    * width: Number,
-    * height: Number,
-    * image: String,
-    * position: Vector,
-    * clickable: Boolean,
-    * value: any,
-    * children: Array<any>,
-    * onHover: function():void,
-    * onNotHover: function():void
-    * }} config Config
-    */
+   * width: number,
+   * height: number,
+   * image: string,
+   * position: Vector,
+   * clickable: boolean,
+   * value: any,
+   * children: Array<any>,
+   * onHover: function():void,
+   * onNotHover: function():void
+   * }} config Config.
+   */
   constructor(config) {
     this.width = config.width || 133;
     this.height = config.height || 25;
@@ -30,6 +33,7 @@ class UIElement {
     this.value = config.value;
 
     this.position = config.position || Vector.zero();
+    // TODO: Update these default functions to something more useful.
     this.onHover = config.onHover || function onHover() { return false; };
     this.onNotHover =
       config.onNotHover || function onNotHover() { return false; };
@@ -39,25 +43,27 @@ class UIElement {
     this.hovered = false;
   }
   /**
-   * Test if the mouse is inside the UI element
+   * Test if the mouse is inside the UI element.
    * @param {{
-   * mouseX: Number,
-   * mouseY: Number
-   * }} event The mouse event that happened
-   * @returns {Boolean}
-  */
+   * mouseX: number,
+   * mouseY: number
+   * }} event The mouse event that happened.
+   * @returns {boolean}
+   */
   isMouseInside(event) {
+    // TODO: Clean up this expression.
     return event.mouseX > this.position.x &&
          event.mouseX < this.position.x + this.width &&
          event.mouseY > this.position.y &&
          event.mouseY < this.position.y + this.height;
   }
   /**
-   * Handles a mouse event
+   * Handles a mouse event.
    * @param {{
-   * mouseX: Number,
-   * mouseY: Number
-   * }} event The mouse event that happened
+   * mouseX: number,
+   * mouseY: number,
+   * clicked: boolean
+   * }} event The mouse event that happened.
    */
   handleMouseEvent(event) {
     if (this.isMouseInside(event)) {
@@ -68,7 +74,7 @@ class UIElement {
   }
   /**
    * Updates the stuff inside the ```children``` property of
-   * this UIElement
+   * this UIElement.
    */
   updateChildren() {
     for (const child of this.children) {
@@ -82,6 +88,6 @@ class UIElement {
 }
 
 /**
- * Module exports
+ * Module exports.
  */
 module.exports = exports = UIElement;
