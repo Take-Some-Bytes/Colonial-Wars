b3053afcd428a1a034f9c4f4ed3ebe5e5c19dba4
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-09-08 17:46:13 -0700

    v0.4.1: Update gameCommon.js
    Updated existing comments and added new ones for more clarity. Also added some codetags.

--- Lib/common/gameCommon.js
+++ Lib/common/gameCommon.js
@@ -3,31 +3,33 @@
  * @author Horton Cheng <horton0712@gmail.com>
  */
 
+// Imports.
 const Vector = require("../Game/Physics/Vector");
 const Icon = require("../Game/UI/Icon");
 const Button = require("../Game/UI/Button");
 const Constants = require("./constants");
 
+// JSDoc typedefs for VSCode.
 /**
  * @typedef {"buttons"|"icons"} UITypes
  */
 /**
  * @typedef {Object} Size
- * @prop {Number} width
- * @prop {Number} height
+ * @prop {number} width
+ * @prop {number} height
  */
 /**
  * @typedef {Object} PopulationStats
- * @prop {Number} used
- * @prop {Number} max
+ * @prop {number} used
+ * @prop {number} max
  */
 /**
  * @typedef {Object} ResourceStats
- * @prop {Number} wood
- * @prop {Number} stone
- * @prop {Number} food
- * @prop {Number} coins
- * @prop {Number} ammo
+ * @prop {number} wood
+ * @prop {number} stone
+ * @prop {number} food
+ * @prop {number} coins
+ * @prop {number} ammo
  */
 /**
  * @typedef {Object} UIOptions
@@ -40,20 +42,25 @@ const Constants = require("./constants");
  * @prop {PopulationStats} [playerStats.population]
  */
 /**
+ * @typedef {Object} ScreenSize
+ * @prop {number} width
+ * @prop {number} height
+ */
+/**
  * @typedef {Object} ScaleOptions Translate options. If the first
  * two properties are omitted, then the last two are required.
  * @prop {ScreenSize} [baseScreenSize] The base screen size to
  * translate the value to.
  * @prop {ScreenSize} [otherScreenSize] The other screen size that
  * the value is coming from.
- * @prop {Number|ScaleStats} [scaleBy] The scale by which to calculate by.
+ * @prop {number} [scaleBy] The scale by which to calculate by.
  */
 
 /**
- * Gets the euclidean distance squared
- * @param {Vector} p1 Point number 1
- * @param {Vector} p2 Point number 2
- * @returns {Number}
+ * Gets the euclidean distance squared.
+ * @param {Vector} p1 Point number 1.
+ * @param {Vector} p2 Point number 2.
+ * @returns {number}
  */
 function getEuclideanDist2(p1, p2) {
   const distance = Math.sqrt(
@@ -70,18 +77,21 @@ function getEuclideanDist2(p1, p2) {
   return distance;
 }
 /**
- * Tests if a point is in a circle
- * @param {Vector} circlePosition The circle's position
- * @param {Number} circleRadius The circle's radius
- * @param {Vector} pointPosition The point that you want to test
- * @returns {Boolean}
+ * Tests if a point is in a circle.
+ * @param {Vector} circlePosition The circle's position.
+ * @param {number} circleRadius The circle's radius.
+ * @param {Vector} pointPosition The point that you want to test.
+ * @returns {boolean}
  */
 function inCircle(circlePosition, circleRadius, pointPosition) {
+  // TODO: See if this is correct. Probably not.
   const euclideanDist = getEuclideanDist2(pointPosition, circlePosition);
 
   const isInCircle = euclideanDist < circleRadius;
   return isInCircle;
 }
+// TODO: Remove the following function. We will be moving UI management
+// to the client side.
 /**
  * Makes the UI for a player.
  * @param {UITypes} type The type of UI to make.
@@ -94,7 +104,7 @@ function makeUI(type, opts) {
     opts.startPosition = {};
   }
   if (type === "buttons") {
-    // Declaration stuff
+    // Declarations.
     const buttonKeys = Constants.BUTTON_KEYS;
     const buttonY = opts.startPosition.y || 570;
     let buttonX = opts.startPosition.x || 1100;
@@ -124,18 +134,19 @@ function makeUI(type, opts) {
       buttonX += 80;
     }
   } else if (type === "icons") {
-    // Declaration stuff
+    // Declarations.
     const iconKeys = Constants.ICON_KEYS;
     const textOffset = new Vector(50, 30);
     const textOffsetHeight = new Vector(-28, 20);
     const iconsY = opts.startPosition.x || 15;
     let iconsX = opts.startPosition.x || 685;
-    // Add icons
+    // Add icons, except for the people icon.
     for (const icon of iconKeys) {
       const infoText = Constants.ICON_INFO_TEXT[icon];
       const value = opts.playerStats.resources[icon];
       const valueIncrease = opts.playerStats.resourceRate[icon];
 
+      // FIXME: See if the following if statement should use `continue`.
       if (icon === "people") { break; }
       const iconToCreate = new Icon({
         image: icon,
@@ -160,6 +171,7 @@ function makeUI(type, opts) {
       iconsX += 110;
     }
 
+    // NOW create the people icon.
     const iconToCreate = new Icon({
       image: "people",
       position: new Vector(iconsX, iconsY),
@@ -190,7 +202,7 @@ function makeUI(type, opts) {
 }
 
 /**
- * Module exports
+ * Module exports.
  */
 module.exports = exports = {
   getEuclideanDist2,
