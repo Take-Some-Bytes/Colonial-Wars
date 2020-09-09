d26155f702567e802ed44bceb40f0044626d61e5
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-09-08 18:17:55 -0700

    v0.4.1: Updated and added comments in Game.js

--- Public/JS/Game/Game.js
+++ Public/JS/Game/Game.js
@@ -1,38 +1,47 @@
 /**
- * @fileoverview Game class for managing client-side game
+ * @fileoverview Game class for managing the client-side game.
  * @author Horton Cheng <horton0712@gmail.com>
  */
-// Imports
+
 import Constants from "../Constants-client.js";
 import Drawing from "./Drawing.js";
 import Input from "./Input.js";
 import Vector from "./Physics/Vector.js";
 import Viewport from "./Viewport.js";
 
+// JSDoc typedefs for easier development.
 /**
  * @typedef {Object} InputState
- * @property {Object} directionData
- * @property {Boolean} directionData.up
- * @property {Boolean} directionData.down
- * @property {Boolean} directionData.right
- * @property {Boolean} directionData.left
- * @property {Object} mouseData
- * @property {Boolean} mouseData.leftMousePressed
- * @property {Boolean} mouseData.rightMousePressed
- * @property {Array<Number>} mouseData.mouseCoords
+ * @prop {Object} directionData
+ * @prop {boolean} directionData.up
+ * @prop {boolean} directionData.down
+ * @prop {boolean} directionData.right
+ * @prop {boolean} directionData.left
+ * @prop {Object} mouseData
+ * @prop {boolean} mouseData.leftMousePressed
+ * @prop {boolean} mouseData.rightMousePressed
+ * @prop {Array<number>} mouseData.mouseCoords
  */
+// Not sure why we have to do this twice, but only this works.
 /**
- * Game class
+ * @typedef {import("socket.io-client")} SocketIOStatic
+ * @typedef {SocketIOStatic} SocketIOClient
+ */
+
+/**
+ * Game class.
  */
 export default class Game {
   /**
-   * Constructor for a Game class
-   * @param {Socket} socket The socket object associated with this player
-   * @param {Viewport} viewport Viewport object
-   * @param {Drawing} drawing Drawing object
-   * @param {Input} input Input object
-   * @param {String} id The ID of the server-side game this client is
-   * connected to
+   * Constructor for a Game class.
+   * @class
+   * @param {SocketIOClient.Socket} socket
+   * The socket object associated with this player.
+   * @param {Viewport} viewport Viewport object.
+   * @param {Drawing} drawing Drawing object.
+   * @param {Input} input Input object.
+   * @param {string} id The ID of the server-side game this client is
+   * connected to.
    */
   constructor(socket, viewport, drawing, input, id) {
     this.socket = socket;
@@ -46,6 +55,8 @@ export default class Game {
     this.troops = [];
     this.buildings = [];
 
+    // TODO: Remove all these properties for now. We won't be needing
+    // them yet. And, UI management is going to be done in another class.
     this.buttons = [];
     this.icons = [];
     this.resources = {};
@@ -60,7 +71,7 @@ export default class Game {
     this.animationFrameId = null;
   }
   /**
-   * Initializes the Game object and binds the socket event listener.
+   * Initializes the Game object and binds the socket and input event listener.
    */
   init() {
     this.lastUpdateTime = Date.now();
@@ -101,6 +112,7 @@ export default class Game {
       this.drawing.clear();
 
       this.drawing.drawTiles();
+      // TODO: Comment out all the following code. We don't need it right now.
       this.buildings.forEach(this.drawing.drawBuilding.bind(this.drawing));
       this.uiBackgrounds.forEach(
         this.drawing.drawUIBackground.bind(this.drawing)
@@ -110,8 +122,8 @@ export default class Game {
     }
   }
   /**
-   * Does stuff when the game receives an update from the server
-   * @param {{}} data The received state
+   * Handles the receiving of the game state from the server.
+   * @param {string} data The received state
    */
   onReceiveGameState(data) {
     const parsedData = JSON.parse(data);
@@ -121,6 +133,7 @@ export default class Game {
     this.self = parsedData.gameData.self;
     // this.players = state.players;
     // this.projectiles = state.projectiles;
+    // TODO: Comment out the rest of the property assigning.
     this.buildings = gameData.buildings;
     // this.troops = state.troops;
     this.buttons = playerData.playerUi[0].children;
@@ -133,8 +146,8 @@ export default class Game {
     this.viewport.updateTrackingPosition(parsedData.gameData.self);
   }
   /**
-   * Updates this game object on input
-   * @param {InputState} state The current input state
+   * Updates this game object on input from the client.
+   * @param {InputState} state The current input state.
    */
   onInput(state) {
     if (this.self) {
@@ -166,22 +179,23 @@ export default class Game {
       }));
     }
   }
+  // TODO: Remove the following function. It is not needed.
   /**
-   * Resets this client's token
-   * @param {String} newToken The new token for the client
+   * Resets this client's token.
+   * @param {string} newToken The new token for the client.
    */
   resetToken(newToken) {
     this.token = newToken;
   }
   /**
    * Factory method for creating a Game class instance.
-   * @param {Socket} socket The socket connected to the server
-   * @param {String} canvasElementID The ID of the canvas element to render the
-   * game to
-   * @param {String} mapName The map's name of the game that the
-   * client is playing on
-   * @param {String} id The ID of the server-side game this client is
-   * connected to
+   * @param {SocketIOClient.Socket} socket The socket connected to the server.
+   * @param {string} canvasElementID The ID of the canvas element to render the
+   * game to.
+   * @param {string} mapName The map's name of the game that the
+   * client is playing on.
+   * @param {string} id The ID of the server-side game this client is
+   * connected to.
    * @returns {Game}
    */
   static create(
