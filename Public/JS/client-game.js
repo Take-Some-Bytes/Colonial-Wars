803317cd07a3a615e15dcfa1da5b5df8115d8c2e
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-09-30 21:00:48 -0700

    v0.4.3: Updated client-game.js
    Updated JSDocs, replaced deprecated features with appropriate new ones, did some TODOs.

--- Public/JS/client-game.js
+++ Public/JS/client-game.js
@@ -1,11 +1,12 @@
 /**
- * @fileoverview This is the client main file for the game page. This file
- * should also be one of the only files to disable eslint-no-def.
+ * @fileoverview This is the client main file for the game page.
  * @author Horton Cheng <horton0712@gmai.com>
  */
-// TODO: Remove the need to disable `no-undef` for this entire file.
-// TODO: Get type definitions for Socket.IO client.
-/* eslint-disable no-undef */
+/**
+ * Get the type definitions
+ * @typedef {import("jquery")} jQuery
+ * @typedef {import("socket.io-client")} SocketIOStatic
+ */
 
 import Constants from "./Constants-client.js";
 import Game from "./Game/Game.js";
@@ -14,18 +15,9 @@ import { changeViewportStats } from "./common/functions.js";
 const pathname = window.location.pathname;
 let game = null;
 
-// TODO: Remove the old security ways.
-window.securityData = {};
-
 if (pathname === "/play") {
   // Connect to the /play namespace.
   const socket = io("/play", {
-    query: {
-      prevSocketID:
-        // TODO: Remove the `prevSocketID` item in the query.
-        // This type of security is not used anymore.
-        localStorage.getItem("prevSocketID")
-    },
     reconnection: false,
     autoConnect: false,
     reconnectionAttempts: 5
@@ -46,13 +38,6 @@ if (pathname === "/play") {
     changeViewportStats();
 
     socket.emit(Constants.SOCKET_SCREEN_RESIZE, JSON.stringify({
-      securityData: {
-        // TODO: Stop using "securityData".
-        clientData: {
-          id: securityData.clientData.id,
-          token: securityData.clientData.token
-        }
-      },
       gameData: {},
       otherData: {
         screen_size: {
@@ -62,26 +47,20 @@ if (pathname === "/play") {
       }
     }));
   });
-  $(document).ready(() => {
+  $(() => {
     $("body")
       .height(Constants.VIEWPORT_HEIGHT)
       .width(Constants.VIEWPORT_WIDTH);
     $("main")
       .height(Constants.VIEWPORT_HEIGHT)
       .width(Constants.VIEWPORT_WIDTH);
-    // BUG: This also doesn't seem to be working.
-    $("#reconnect").click(() => {
-      socket.connect();
-    });
 
-    // TODO: Change the error messages so that they make more sense.
     socket.on(Constants.SOCKET_DISCONNECT, reason => {
       if (reason === "io server disconnect") {
         $("body")
           .html("")
           .html(
-            "<h1>Connection lost. Click <a id=\"reconnect\">" +
-            "here</a> to reconnect.</h1>"
+            "<h1>Connection lost."
           );
         return;
       }
@@ -112,7 +91,7 @@ if (pathname === "/play") {
       $("body")
         .html("")
         .html(
-          "<h1>The server has rejected your request. Click <a href=\"/\">" +
+          "<h1>An error has occured. Click <a href=\"/\">" +
           "here</a> to go back to the main page.</h1>\n" +
           `<h3>Error is: ${err}</h3>`
         );
@@ -120,14 +99,6 @@ if (pathname === "/play") {
 
     socket.connect();
 
-    // TODO: This shouldn't be needed anymore. Remove it.
-    socket.on(Constants.SOCKET_SECURITY_DATA, data => {
-      window.securityData = JSON.parse(data).securityData;
-
-      if (game && game instanceof Game) {
-        game.resetToken(window.securityData.clientData.token);
-      }
-    });
     // We have to wait for a "proceed" event to make sure that the
     // server is ready for us.
     socket.on(Constants.SOCKET_PROCEED, data => {
